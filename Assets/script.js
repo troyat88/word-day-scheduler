1c9d0ead46ab6d5a4de0ef6bcf692888ed60cc5d
Troy <troyat88@gmail.com>
2021-07-18 13:38:44 -0700

    fixed color code render saved event bug

--- Assets/script.js
+++ Assets/script.js
@@ -51,31 +51,31 @@ $(".saveBtn").click(function(){
 
 //function renderEvents(){
     var savedEvent = localStorage.getItem("8AM");
-    $("#0800").val(savedEvent);
+    $("#8").val(savedEvent);
     var savedEvent1 = localStorage.getItem("9AM");
-    $("#0900").val(savedEvent1);
+    $("#9").val(savedEvent1);
     var savedEvent2 = localStorage.getItem("10AM");
-    $("#1000").val(savedEvent2);
+    $("#10").val(savedEvent2);
     var savedEvent3 = localStorage.getItem("11AM");
-    $("#1100").val(savedEvent3);
+    $("#11").val(savedEvent3);
     var savedEvent4 = localStorage.getItem("12PM");
-    $("#1200").val(savedEvent4);
+    $("#12").val(savedEvent4);
     var savedEvent5 = localStorage.getItem("1PM");
-    $("#1300").val(savedEvent5);
+    $("#13").val(savedEvent5);
     var savedEvent6 = localStorage.getItem("2PM");
-    $("#1400").val(savedEvent6);
+    $("#14").val(savedEvent6);
     var savedEvent7 = localStorage.getItem("3PM");
-    $("#1500").val(savedEvent7);
+    $("#15").val(savedEvent7);
     var savedEvent8 = localStorage.getItem("4PM");
-    $("#1600").val(savedEvent8);
+    $("#16").val(savedEvent8);
     var savedEvent9 = localStorage.getItem("5PM");
-    $("#1700").val(savedEvent9);
+    $("#17").val(savedEvent9);
     var savedEvent10 = localStorage.getItem("6PM");
-    $("#1800").val(savedEvent10);
+    $("#18").val(savedEvent10);
     var savedEvent11 = localStorage.getItem("7PM");
-    $("#1900").val(savedEvent11);
+    $("#19").val(savedEvent11);
     var savedEvent12 = localStorage.getItem("8PM");
-    $("#2000").val(savedEvent12);
+    $("#20").val(savedEvent12);
 //}
 //renderEvents()
 
